2fdf74d6d1af1ae2451d5df177e705ada3ecf40e
Siddharta Govindaraj <siddharta@silverstripesoftware.in>
2023-11-28 00:24:00 +0530

    Fixed typo in Jinja2Templates

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from starlette.applications import Starlette
 from starlette.routing import Route
 from starlette.templating import Jinja2Templates
 
-templates = Jinja2Templeates(directory='templates')
+templates = Jinja2Templates(directory='templates')
 
 async def index(request):
     return templates.TemplateResponse(
